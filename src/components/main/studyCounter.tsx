c09783fe07ef5e3150b7ccfb0cedd7950a6b2596
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-10-16 20:48:37 +0900

    イケてるタイマーに変更

--- src/components/main/studyCounter.tsx
+++ src/components/main/studyCounter.tsx
@@ -1,13 +1,39 @@
-import { Button, Center, Stack, Title, Grid } from '@mantine/core'
+import { Button, Center, Stack, Title, Text, Grid, Group, Progress, Card, createStyles } from '@mantine/core'
 import { useCountdownTimer } from 'use-countdown-timer'
 import shallow from 'zustand/shallow'
 import { useSettingsStore } from 'src/stores/settingsStore'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    backgroundColor: theme.fn.primaryColor(),
+    width: '100%'
+  },
+
+  title: {
+    color: theme.fn.rgba(theme.white, 0.65),
+  },
+
+  stats: {
+    color: theme.white,
+  },
+
+  progressBar: {
+    backgroundColor: theme.white,
+  },
+
+  progressTrack: {
+    backgroundColor: theme.fn.rgba(theme.white, 0.4),
+  },
+}));
 
 const StudyCounter = () => {
+  const { classes } = useStyles();
+  const timerSeconds = 60
   const { countdown, start, reset, pause, isRunning } = useCountdownTimer({
-    timer: 1000 * 3,
+    timer: 1000 * timerSeconds,
   });
+  const percentage = useRef(0);
   const { setMode, setPositionX, setPositionY, setPositionZ } = useSettingsStore(
     (state) => ({
       setMode: state.setMode,
@@ -29,13 +55,38 @@ const StudyCounter = () => {
       setPositionZ(0)
       setMode('fitness')
     }
+    percentage.current = countdown * 100 / (timerSeconds * 1000)
   }, [isRunning, countdown])
 
   return (
     <div>
       <Stack sx={() => ({ backgroundColor: 'transparent' })}>
         <Center>
-          <Title>{new Date(countdown).toISOString().slice(14, 19)}</Title>
+          <Card withBorder radius="md" p="xl" className={classes.card}>
+            <Text size="xs" weight={700} className={classes.title}>
+              REMAINING
+            </Text>
+            <Title weight={500} className={classes.stats}>
+              {new Date(countdown).toISOString().slice(14, 19)}
+            </Title>
+
+            <Progress
+              value={percentage.current}
+              mt="md"
+              size="lg"
+              radius="xl"
+              classNames={{
+                root: classes.progressTrack,
+                bar: classes.progressBar
+              }}
+            />
+            <Group position="left">
+              <Text size="xs" color="white">
+                あと {Math.trunc(percentage.current)}%
+              </Text>
+            </Group>
+          </Card>
+          {/* <Title>{new Date(countdown).toISOString().slice(14, 19)}</Title> */}
         </Center>
         <Grid>
           <Grid.Col span={6}>
