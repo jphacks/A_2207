a1d816b1a38913552102642cce1dec3d8510891a
Kitsuya0828 <kitsuyaazuma@gmail.com>
2022-10-22 05:08:55 +0900

    25分デフォでやったときzustandの初期化が1分になっている

--- src/stores/settingsStore.ts
+++ src/stores/settingsStore.ts
@@ -25,7 +25,7 @@ export const initialState: SettingsState = {
   mode: 'initial',
   goal: '',
   studied: false,
-  countRemain: 60,
+  countRemain: 60*25,
   squatGoalCount: 15,
   workTime: 25,
   breakTime: 5,
